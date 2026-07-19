37a090f5ebde10e2aa2978ba96256f4caad09586
Taranom <trnmbghry@gmail.com>
2026-07-19 15:07:46 +0330

    update

--- onlineshop/Authentication/urls.py
+++ onlineshop/Authentication/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 from User.views import RegisterUserAPI
+from .views import LogoutView
 
 urlpatterns = [
     path('login/',jwt_views.TokenObtainPairView.as_view()),
     path('refresh/',jwt_views.TokenRefreshView.as_view()),
     path('register/', RegisterUserAPI.as_view()),
+    path('logout',LogoutView.as_view(), name='auth_logout'),
 
 
 ]
+
+
--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'User',
     'Authentication',
     'Category',
@@ -150,4 +151,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=35),}
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=35),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True
+}
