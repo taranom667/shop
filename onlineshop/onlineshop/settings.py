19aaa65f9c79bfeefd4f8a7bab3981acda20c282
Taranom <trnmbghry@gmail.com>
2026-09-01 18:54:14 +0330

    update

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -26,14 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c%d@rtcz5y6*ogoj(fjy!=4tfpb90a@-kyo&-_yblm*6%qzox='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
